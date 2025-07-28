c8eb99ac1248ad872aabb919e2e0b6616c377924
C7-mia <akidirita4@gmail.com>
2025-07-28 14:27:03 +0300

    Update searchBar.jsx
    Adding filters and triggering filters each time the searchBar is used

--- recipe-sharing-app/src/components/searchBar.jsx
+++ recipe-sharing-app/src/components/searchBar.jsx
@@ -3,12 +3,19 @@ import { useRecipeStore } from './recipeStore';
 
 const SearchBar = () => {
   const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+  const filterRecipes = useRecipeStore((state) => state.filterRecipes); // 
+
+  const handleSearch = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    filterRecipes(); // 
+  };
 
   return (
     <input
       type="text"
       placeholder="Search recipes..."
-      onChange={(e) => setSearchTerm(e.target.value)}
+      onChange={handleSearch}
       style={{
         width: '100%',
         padding: '10px',
