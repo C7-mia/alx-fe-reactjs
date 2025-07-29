438f5a77b62d7bd4cca37eafe90b60a629654dae
C7-mia <akidirita4@gmail.com>
2025-07-29 13:40:45 +0300

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,10 +1,12 @@
-// src/App.jsx
 import React from 'react';
-import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = {
+    name: 'John Doe',
+    email: 'john.doe@example.com'
+  };
 
   return (
     <UserContext.Provider value={userData}>
