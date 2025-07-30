fc90eace75ea51f50cd0a0eb3e55bdae636a8c12
C7-mia <akidirita4@gmail.com>
2025-07-31 00:40:43 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,21 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
+import RecipeDetail from './components/RecipeDetail'; // Create a stub if it doesn't exist
 
 function App() {
   return (
-    <div style={{ padding: '1rem', maxWidth: '700px', margin: 'auto' }}>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-      <hr />
-      <FavoritesList />
-      <hr />
-      <RecommendationsList />
-    </div>
+    <Router>
+      <div style={{ padding: '1rem', maxWidth: '700px', margin: 'auto' }}>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+                <hr />
+                <FavoritesList />
+                <hr />
+                <RecommendationsList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
