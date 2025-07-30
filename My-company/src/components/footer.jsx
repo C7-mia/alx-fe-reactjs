c267e0540caa3ca68965e81d9b0435559ea27fab
C7-mia <akidirita4@gmail.com>
2025-07-30 23:51:36 +0300

    Delete My-company/src directory

--- My-company/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', padding: '20px', marginTop: '40px', background: '#eee' }}>
-      <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
