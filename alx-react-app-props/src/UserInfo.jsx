2ac4e8478de7c9f5e2de932b3471a9f1ee0356ab
C7-mia <akidirita4@gmail.com>
2025-07-30 21:16:23 +0300

    Update UserInfo.jsx

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
 import UserDetails from './UserDetails';
 
 function UserInfo() {
-  return (
-    <div>
-      <h3>User Info</h3>
-      <UserDetails />
-    </div>
-  );
+  return <UserDetails />;
 }
 
 export default UserInfo;
