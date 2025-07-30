75a6e63924ba50cb83f9f537457999eab53601d0
C7-mia <akidirita4@gmail.com>
2025-07-30 23:47:09 +0300

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,10 @@
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
