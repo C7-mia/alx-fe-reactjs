b22dde0374cb741b89c2fcfff2b8299aab56c0ea
C7-mia <akidirita4@gmail.com>
2025-07-30 20:58:41 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
-import RecipeDetail from './components/RecipeDetail'; // new component
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <Router>
-      <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
-        <h1>Recipe Sharing App</h1>
-        <SearchBar />
-        <AddRecipeForm />
-
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipes/:id" element={<RecipeDetail />} /> {/* Add route */}
-        </Routes>
-      </div>
-    </Router>
+    <div style={{ padding: '1rem', maxWidth: '700px', margin: 'auto' }}>
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <AddRecipeForm />
+      <RecipeList />
+      <hr />
+      <FavoritesList />
+      <hr />
+      <RecommendationsList />
+    </div>
   );
 }
 
