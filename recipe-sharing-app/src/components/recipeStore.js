1741ef200ef6c533010ed7d12e7a0c3d550bea4f
C7-mia <akidirita4@gmail.com>
2025-07-30 13:28:18 +0300

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,69 +1,30 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-const useRecipeStore = create((set, get) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
   searchTerm: '',
   filteredRecipes: [],
-
-  // Actions
-  setRecipes: (newRecipes) => {
-    set({
-      recipes: newRecipes,
-      filteredRecipes: get().filterByTerm(newRecipes, get().searchTerm),
-    });
-  },
-
-  setSearchTerm: (term) => {
-    set({ searchTerm: term });
-    get().filterRecipes();
-  },
-
-  addRecipe: (recipe) => {
-    set((state) => {
-      const updatedRecipes = [...state.recipes, recipe];
-      return {
-        recipes: updatedRecipes,
-        filteredRecipes: get().filterByTerm(updatedRecipes, state.searchTerm),
-      };
-    });
-  },
-
-  deleteRecipe: (id) => {
-    set((state) => {
-      const updatedRecipes = state.recipes.filter((r) => r.id !== id);
-      return {
-        recipes: updatedRecipes,
-        filteredRecipes: get().filterByTerm(updatedRecipes, state.searchTerm),
-      };
-    });
-  },
-
-  updateRecipe: (updatedRecipe) => {
-    set((state) => {
-      const updatedRecipes = state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      );
-      return {
-        recipes: updatedRecipes,
-        filteredRecipes: get().filterByTerm(updatedRecipes, state.searchTerm),
-      };
-    });
-  },
-
-  filterByTerm: (recipes, term) => {
-    return recipes.filter((recipe) =>
-      recipe.title.toLowerCase().includes(term.toLowerCase())
-    );
-  },
-
-  filterRecipes: () => {
-    const { recipes, searchTerm } = get();
-    set({
-      filteredRecipes: recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+      filteredRecipes: [...state.recipes, newRecipe].filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
       ),
-    });
-  },
+    })),
+  setRecipes: (recipes) =>
+    set((state) => ({
+      recipes,
+      filteredRecipes: recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
+    })),
+  setSearchTerm: (term) =>
+    set((state) => ({
+      searchTerm: term,
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      ),
+    })),
 }));
 
 export { useRecipeStore };
