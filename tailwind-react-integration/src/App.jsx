ce3c1cabf29f66bdeadd3bb0f21b7d5ca4838091
C7-mia <akidirita4@gmail.com>
2025-08-04 12:33:49 +0300

    Set up Tailwind CSS in Vite React app

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center text-blue-500 text-3xl font-bold mt-10">
+      Tailwind CSS is working!
+    </div>
+  )
+}
+
+export default App
