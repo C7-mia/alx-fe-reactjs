ae87e63b86423def5e56fa5c2bc935cc3c6019a6
C7-mia <akidirita4@gmail.com>
2025-08-13 12:15:33 +0300

    Create AddRecipe.jsx

---
+++ recipe-sharing-platform/src/pages/AddRecipe.jsx
@@ -0,0 +1,10 @@
+function AddRecipe() {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold">Add a New Recipe</h2>
+      <p className="mt-2 text-gray-600">Form to submit your recipe will be here.</p>
+    </div>
+  );
+}
+
+export default AddRecipe;
