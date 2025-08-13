6aaa2950adc03ade372bcd50c403bf935dabdd3d
C7-mia <akidirita4@gmail.com>
2025-08-13 12:12:12 +0300

    Create Footer.jsx

---
+++ recipe-sharing-platform/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+      &copy; {new Date().getFullYear()} Recipe Sharing Platform. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
