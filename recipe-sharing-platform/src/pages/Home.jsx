5ee5623612c7e4142856d3c3b8aca95c172a45f6
C7-mia <akidirita4@gmail.com>
2025-08-13 12:13:22 +0300

    Create Home.jsx

---
+++ recipe-sharing-platform/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold">All Recipes</h2>
+      <p className="mt-2 text-gray-600">Browse and discover amazing recipes.</p>
+    </div>
+  );
+}
+
+export default Home;
