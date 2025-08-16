bb25aa10a06d0d2d640e71753be96e2f24df4947
C7-mia <akidirita4@gmail.com>
2025-08-16 16:21:33 +0300

    integrate React Router with HomePage and RecipeDetail

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
 import './App.css';
-
+<h1 className="text-3xl font-bold text-blue-500">
+  Hello Tailwind!
+</h1>
 function App() {
   return (
     <Router>
