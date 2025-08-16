2da4d3fc0d5d75eb6f6a4c6e63f1e7cd1e29a6ed
C7-mia <akidirita4@gmail.com>
2025-08-16 03:47:54 +0300

    Update tailwind.config.js content paths

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+    }
