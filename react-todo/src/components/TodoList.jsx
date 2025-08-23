23675e6fb9c23dbddce805997f14df682541e6b9
C7-mia <akidirita4@gmail.com>
2025-08-23 23:37:58 +0300

    Create TodoList.jsx

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import AddTodoForm from "./AddTodoForm";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build Todo App", completed: true },
+  ]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => toggleTodo(todo.id)}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text} <button onClick={() => deleteTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+    }
