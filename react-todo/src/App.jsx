c5c9e573a9e4cd78ffd2ea378e77255c63420635
C7-mia <akidirita4@gmail.com>
2025-08-23 23:49:10 +0300

    Create App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,10 @@
+import TodoList from './components/TodoList'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui', padding: 20 }}>
+      <h1>React Todo List</h1>
+      <TodoList />
+    </div>
+  )
+}
