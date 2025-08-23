85fb9c883d0e6a0a4f2d149e80962c9511b5831a
C7-mia <akidirita4@gmail.com>
2025-08-23 23:36:47 +0300

    Update TodoList.test.js

--- react-todo/src/_tests_/TodoList.test.js
+++ react-todo/src/_tests_/TodoList.test.js
@@ -1,1 +1,31 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
 
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build Todo App")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText("New todo"), {
+    target: { value: "Test Todo" },
+  });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("Test Todo")).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getAllByText("X")[0]);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
