4d5571147a2fe18536629d6a1b73cd7aa5a714f9
C7-mia <akidirita4@gmail.com>
2025-08-24 00:44:21 +0300

    Create ProfileSettings.jsx

---
+++ react-router-advanced/src/pages/ProfileSettings.jsx
@@ -0,0 +1,8 @@
+export default function ProfileSettings() {
+  return (
+    <div>
+      <h2>Profile Settings</h2>
+      <p>Change password, notification preferences, and privacy options here.</p>
+    </div>
+  )
+}
