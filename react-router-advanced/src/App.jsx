fe03101f228cab8d94909e629d1fe5bf33c47f67
C7-mia <akidirita4@gmail.com>
2025-08-23 23:11:59 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import { AuthProvider } from './context/AuthContext'
 
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import Post from './pages/Post'
-import BlogPost from './pages/BlogPost'   // import BlogPost
+import BlogPost from './pages/BlogPost'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import ProfileDetails from './pages/ProfileDetails'
@@ -16,32 +16,35 @@ import ProtectedRoute from './routes/ProtectedRoute'
 export default function App() {
   return (
     <AuthProvider>
-      <div style={{ fontFamily: 'system-ui, Arial', padding: 16 }}>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
+      {/*  BrowserRouter wraps everything */}
+      <BrowserRouter>
+        <div style={{ fontFamily: 'system-ui, Arial', padding: 16 }}>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-          {/* Blog Routes */}
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:postId" element={<Post />} />
-          <Route path="/blog/:id" element={<BlogPost />} /> {/*  new dynamic route */}
+            {/* Blog Routes */}
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:postId" element={<Post />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
 
-          {/* Login */}
-          <Route path="/login" element={<Login />} />
+            {/* Login */}
+            <Route path="/login" element={<Login />} />
 
-          {/* Protected + Nested */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/profile" element={<Profile />}>
-              <Route index element={<ProfileDetails />} />
-              <Route path="details" element={<ProfileDetails />} />
-              <Route path="settings" element={<ProfileSettings />} />
+            {/* Protected + Nested */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<Profile />}>
+                <Route index element={<ProfileDetails />} />
+                <Route path="details" element={<ProfileDetails />} />
+                <Route path="settings" element={<ProfileSettings />} />
+              </Route>
             </Route>
-          </Route>
 
-          {/* Not Found */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+            {/* Not Found */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   )
-  }
+      }
