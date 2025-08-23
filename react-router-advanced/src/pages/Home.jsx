c4665c270199369eca616917e6bf4016cf2103d1
C7-mia <akidirita4@gmail.com>
2025-08-23 16:05:32 +0300

    Create Home.jsx

---
+++ react-router-advanced/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section>
+      <h1>Home</h1>
+      <p>Welcome! Explore Blog, try Profile (protected), and log in to access nested routes.</p>
+    </section>
+  )
+}
